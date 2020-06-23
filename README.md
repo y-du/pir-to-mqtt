030679acc42cd717c9cc2613e1411403103fc229
y-du <42994541+y-du@users.noreply.github.com>
2020-06-23 21:29:58 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+config.h
+
+	#define WIFI_SSID "Your SSID"
+	#define WIFI_PASSWORD "Your Password"
+	#define MQTT_SERVER "IP Address of MQTT Server"
+	#define MQTT_PORT 1883
+	#define MQTT_RECONNECT 5000
+	#define MQTT_CLIENT_ID "PIR-"
+	#define MQTT_TOPIC "event"
+	#define PIR_ID "PIR001"
+	#define SRV_ID "motion"
+	#define PUP_INTERVAL 1500
+	#define PIR_INIT_TIME 45000
---
+++ pir-to-mqtt.ino
@@ -0,0 +1,90 @@
+#include "config.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+
+const byte pir_pin{3};
+const byte led_pin{1};
+String client_id{MQTT_CLIENT_ID};
+
+
+WiFiClient esp_client;
+PubSubClient mqtt_client(esp_client);
+
+
+void setupWifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+     digitalWrite(led_pin, LOW);
+     delay(250);
+     digitalWrite(led_pin, HIGH);
+     delay(250);
+  }
+}
+
+
+boolean reconnect() {
+    digitalWrite(led_pin, LOW);
+    mqtt_client.connect(client_id.c_str());
+    digitalWrite(led_pin, HIGH);
+    return mqtt_client.connected();
+  }
+
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+  digitalWrite(led_pin, HIGH);
+  pinMode(pir_pin, INPUT);
+  setupWifi();
+  randomSeed(micros());
+  client_id += String(random(0xffff), HEX);
+  mqtt_client.setServer(MQTT_SERVER, MQTT_PORT);
+}
+
+
+unsigned long now{0};
+unsigned long last_reconnect{0};
+byte motion{false};
+byte pir_init{false};
+unsigned long last_msg{0};
+String topic{String() + MQTT_TOPIC + "/" + PIR_ID + "/" + SRV_ID};
+
+
+void loop() {
+  if (!mqtt_client.connected()) {
+    now = millis();
+    if (now - last_reconnect > MQTT_RECONNECT) {
+      last_reconnect = now;
+      if (reconnect()) {
+        last_reconnect = 0;
+      }
+    }
+  } else {
+    mqtt_client.loop();
+    if (pir_init) {
+      if (digitalRead(pir_pin) == HIGH) {
+        if (motion == false) {
+          motion = true;
+        }
+        now = millis();
+        if (now - last_msg >= PUP_INTERVAL) {
+          mqtt_client.publish(topic.c_str(), "1");
+          last_msg = now;
+        }
+      } else {
+        if (motion == true) {
+          motion = false;
+          last_msg = 0;
+        }
+      }
+    } else {
+      if (millis() >= PIR_INIT_TIME) {
+        pir_init = true;
+        digitalWrite(led_pin, LOW);
+        delay(20);
+        digitalWrite(led_pin, HIGH);
+      }
+    }
+  }
+}
